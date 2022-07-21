d98c6e1fb6ac5defd38406a7e64885a669b40718
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2022-07-21 09:28:43 +0700

    development: setup firebase - add feature

--- src/components/DetailsBanner.jsx
+++ src/components/DetailsBanner.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
+import PlayIcon from "@mui/icons-material/PlayArrow";
 
 export default function DetailsBanner({ data }) {
+  var formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+
+    maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  });
   return data ? (
     <Box
       sx={{
@@ -40,14 +47,23 @@ export default function DetailsBanner({ data }) {
               alt=""
             />
           </Box>
-          <Box sx={{ width: "60%", padding: "1rem", boxSizing: "border-box" }}>
+          <Box
+            sx={{
+              width: "60%",
+              padding: "3rem 2rem",
+              boxSizing: "border-box",
+              display: "flex",
+              flexFlow: "column",
+              justifyContent: "space-between",
+              height: "80vh",
+            }}
+          >
             <Typography
               color="white"
               sx={{
                 fontWeight: "700",
                 fontSize: "2rem",
-                lineHeight: "3.5rem",
-                marginBottom: "1rem",
+                lineHeight: "3rem",
               }}
             >
               {data?.original_title}{" "}
@@ -61,6 +77,52 @@ export default function DetailsBanner({ data }) {
                 ({data?.release_date?.slice(0, 4)})
               </span>
             </Typography>
+            <Typography variant="body1" color="white" fontSize={"0.9rem"}>
+              {data?.release_date}{" "}
+              {data?.genres.map((data) => `| ${data.name} `)}
+              {" - "}
+              {`${Math.floor(data?.runtime / 60)}h`} {`${data?.runtime % 60}m`}
+            </Typography>
+            <Typography color="white">{data?.tagline}</Typography>
+            <div style={{ marginTop: "2rem" }}>
+              <Typography variant="h5" fontWeight={"500"} color="white">
+                Overview
+              </Typography>
+              <Typography color="white">{data?.overview}</Typography>
+            </div>
+            <Typography sx={{ marginTop: "1rem" }} color="white">
+              {`Budget ${formatter.format(
+                data?.budget
+              )} | Revenue ${formatter.format(data?.revenue)}`}
+            </Typography>
+            <Typography sx={{ marginTop: "1rem" }} color="white">
+              Status {data?.status}
+            </Typography>
+            <div style={{ marginTop: "1rem" }}>
+              <Typography variant="h6" fontWeight={"500"} color="white">
+                Languages
+              </Typography>
+              <Typography color="white">
+                {data?.spoken_languages?.map(
+                  (data) => `| ${data.english_name} `
+                )}
+              </Typography>
+            </div>
+            <div style={{ marginTop: "1rem" }}>
+              <Button
+                variant="outlined"
+                sx={{
+                  borderRadius: 0,
+                  border: "1px solid white",
+                  color: "white",
+                }}
+                target="_blank"
+                href={data?.homepage}
+                startIcon={<PlayIcon />}
+              >
+                Play
+              </Button>
+            </div>
           </Box>
         </Box>
       </Box>
