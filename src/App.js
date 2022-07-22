353460dbdc00271f2e7605d6e2f8fb9dc82fb217
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2022-07-22 11:18:37 +0700

    main: setting up router & create useScrollToTop hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import Routes from "./routes/Routers.js";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
 
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <Routes />
-      </Provider>
+      <Routes />
     </div>
   );
 }
--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Detailpage from "../pages/Detailpage";
 import Homepage from "../pages/Homepage";
 import LoginPage from "../pages/LoginPage";
@@ -8,55 +8,56 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../app/firebase";
 import Seriespage from "../pages/Seriespage";
 import Moviespage from "../pages/Moviespage";
+import useScrollToTop from "../hooks/useScrollToTop";
 
 export default function Routers() {
+  useScrollToTop();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route
-          path="/details/:media_type/:id"
-          element={
-            <PrivateRoute>
-              <Detailpage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/series"
-          element={
-            <PrivateRoute>
-              <Seriespage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/movies"
-          element={
-            <PrivateRoute>
-              <Moviespage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <ProtectLoginRegister>
-              <LoginPage />
-            </ProtectLoginRegister>
-          }
-        ></Route>
-        <Route
-          path="/register"
-          element={
-            <ProtectLoginRegister>
-              <RegisterPage />
-            </ProtectLoginRegister>
-          }
-        ></Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route
+        path="/details/:media_type/:id"
+        element={
+          <PrivateRoute>
+            <Detailpage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/series"
+        element={
+          <PrivateRoute>
+            <Seriespage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/movies"
+        element={
+          <PrivateRoute>
+            <Moviespage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <ProtectLoginRegister>
+            <LoginPage />
+          </ProtectLoginRegister>
+        }
+      ></Route>
+      <Route
+        path="/register"
+        element={
+          <ProtectLoginRegister>
+            <RegisterPage />
+          </ProtectLoginRegister>
+        }
+      ></Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
 
